b5e053939d8def4c8a9fffc36c0103ba7b58d411
sunkaichang <sunkaichang@sina.com>
2023-06-26 16:40:55 +0800

    Update model-api.go
    modi status type

--- pfc/go1688/model-api.go
+++ pfc/go1688/model-api.go
@@ -112,8 +112,8 @@ type ProtocolPayIsOpenResponse struct {
 	Code string `json:"code"` // 错误码
 }
 type IsOpenPaymentAgreements struct {
-	BindingStatus string `json:"bindingStatus"`     // 支付宝或者诚E赊是否已设置绑定，signedStatus和bindingStatus均为true才能发起代扣
-	PayChannel    string `json:"payChannel"`        // 支付成功的渠道，支付不成功渠道为null
-	SignedStatus  string `json:"signedStatus"`      // 签约URl
-	SignUrl       string `json:"signUrl,omitempty"` // 签约单号
+	BindingStatus bool   `json:"bindingStatus"` // 支付宝或者诚E赊是否已设置绑定，signedStatus和bindingStatus均为true才能发起代扣
+	PayChannel    string `json:"payChannel"`    // 支付成功的渠道，支付不成功渠道为null
+	SignedStatus  bool   `json:"signedStatus"`  // 签约URl
+	SignUrl       string `json:"signUrl"`       // 签约单号
 }
